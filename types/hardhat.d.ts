231ac7af2b93ea702f0a705457b0624e132b9d41
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-07-11 09:18:27 -0400

    prep for packaging

---
+++ types/hardhat.d.ts
@@ -0,0 +1,9 @@
+// Type augmentation for Hardhat Runtime Environment
+// import "@nomicfoundation/hardhat-toolbox";
+// import { HardhatEthersHelpers } from "@nomicfoundation/hardhat-ethers/types";
+
+// declare module "hardhat/types/runtime" {
+//   interface HardhatRuntimeEnvironment {
+//     ethers: HardhatEthersHelpers;
+//   }
+// }
